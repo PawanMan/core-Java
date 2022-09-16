c4a8548309d5bc873bc61aec314182052cfabd97
PawanMan <pawan.mandloi05@gmail.com>
2022-09-17 00:47:03 +0530

    message

---
+++ basics/Incement.java
@@ -0,0 +1,9 @@
+package com.rays.basics;
+
+public class Incement {
+	public static void main(String[] args) {
+		int i=0;
+		System.out.println(i++ + i++ + ++i + ++i + i++);
+	}
+
+}
---
+++ basics/TestDate.java
@@ -0,0 +1,12 @@
+package com.rays.basics;
+
+import java.time.LocalDate;
+
+public class TestDate {
+public static void main(String[] args) {
+	LocalDate LD=LocalDate.now();
+	System.out.println(LD);
+}
+
+
+}
